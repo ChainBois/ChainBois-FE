d8760230e244cf075b5e5a11fdc31251e7a4692a
Emmanuel Agbavwe <agbavweisaac@gmail.com>
2025-09-18 06:24:16 +0100

    update: add border-like gradiant effect to tournament cards, implement backup plan to slow loading images on both tournament and team member cards

--- src/components/TournamentCard/TournamentCard.jsx
+++ src/components/TournamentCard/TournamentCard.jsx
@@ -85,98 +85,110 @@ export default function TournamentCard() {
 		<article
 			className={cf(
 				s.flex,
-				s.spaceXEnd,
-				s.spaceYStart,
-				s.p_relative,
+				s.flexCenter,
 				t.tournamentCard
 			)}
 		>
-			<Image
-				src={Ruins}
-				alt='Ruins'
-				className={t.card__img}
-			/>
-			<div className={cf(t.card__percent)}>
-				<svg className={cf(t.svg)}>
-					<defs>
-						<radialGradient
-							id='gradient'
-							cx='50%'
-							cy='50%'
-							r='60%'
-							fx='50%'
-							fy='50%'
-						>
-							<stop
-								offset='30%'
-								stopColor='var(--primary-dark)'
-							/>
-							<stop
-								offset='100%'
-								stopColor='var(--primary-light)'
-							/>
-						</radialGradient>
-					</defs>
-					<circle
-						cx='51.5'
-						cy='51.5'
-						r='42.5'
-						stroke='url(#gradient)'
-						id='circle'
-						ref={circle}
-						className={cf(t.circle)}
-					></circle>
-				</svg>
-				<div
-					className={cf(s.flex, s.flex_dColumn, s.flexStart, t.card__number)}
-				>
-					<span className={cf(s.wMax, s.tCenter, s.dInlineBlock, t.progress)}>
-						{progress}
-					</span>
-					<span className={cf(s.wMax, s.tCenter, s.dInlineBlock, t.days)}>
-						Day{progress !== 1 ? 's' : ''}
-					</span>
+			<div
+				className={cf(
+					s.wMax,
+					s.hMax,
+					s.flex,
+					s.spaceXEnd,
+					s.spaceYStart,
+					s.p_relative,
+					t.card__container
+				)}
+			>
+				<Image
+					src={Ruins}
+					alt='Ruins'
+					className={t.card__img}
+				/>
+				<div className={cf(t.card__percent)}>
+					<svg className={cf(t.svg)}>
+						<defs>
+							<radialGradient
+								id='gradient'
+								cx='50%'
+								cy='50%'
+								r='60%'
+								fx='50%'
+								fy='50%'
+							>
+								<stop
+									offset='30%'
+									stopColor='var(--primary-dark)'
+								/>
+								<stop
+									offset='100%'
+									stopColor='var(--primary-light)'
+								/>
+							</radialGradient>
+						</defs>
+						<circle
+							cx='51.5'
+							cy='51.5'
+							r='42.5'
+							stroke='url(#gradient)'
+							id='circle'
+							ref={circle}
+							className={cf(t.circle)}
+						></circle>
+					</svg>
+					<div
+						className={cf(s.flex, s.flex_dColumn, s.flexStart, t.card__number)}
+					>
+						<span className={cf(s.wMax, s.tCenter, s.dInlineBlock, t.progress)}>
+							{progress}
+						</span>
+						<span className={cf(s.wMax, s.tCenter, s.dInlineBlock, t.days)}>
+							Day{progress !== 1 ? 's' : ''}
+						</span>
+					</div>
 				</div>
-			</div>
 
-			<div className={cf(s.flex, s.flexStart, s.p_relative, t.tournamentInfo)}>
-				<h3 className={cf(t.tournamentTitle)}>Survival Battle</h3>
-				<p className={cf(t.tournamentLore)}>
-					The point of using Lorem Ipsum is that it has a more-or-less normal
-					distribution of letters, as opposed to using.
-				</p>
-				<BorderedButton
-					tag={'Join Tournament'}
-					icon={<BsCaretRightFill className={cf(t.playIcon)} />}
-					isLink={false}
-					action={() => {}}
-					borderButton={cf(t.borderButton)}
-					borderButtonContent={cf(t.borderButtonContent)}
-					borderButtonText={cf(t.borderButtonText)}
-					borderButtonIcon={cf(t.borderButtonIcon)}
-					type={'button'}
-				/>
-			</div>
+				<div
+					className={cf(s.flex, s.flexStart, s.p_relative, t.tournamentInfo)}
+				>
+					<h3 className={cf(t.tournamentTitle)}>Survival Battle</h3>
+					<p className={cf(t.tournamentLore)}>
+						The point of using Lorem Ipsum is that it has a more-or-less normal
+						distribution of letters, as opposed to using.
+					</p>
+					<BorderedButton
+						tag={'Join Tournament'}
+						icon={<BsCaretRightFill className={cf(t.playIcon)} />}
+						isLink={false}
+						action={() => {}}
+						borderButton={cf(t.borderButton)}
+						borderButtonContent={cf(t.borderButtonContent)}
+						borderButtonText={cf(t.borderButtonText)}
+						borderButtonIcon={cf(t.borderButtonIcon)}
+						type={'button'}
+					/>
+				</div>
 
-			<footer className={cf(s.flex, s.flexStart, t.tournamentMeta)}>
-				<TournamentStat
-					tag={'Competition Status'}
-					value={'Active'}
-					icon={<div className={cf(t.statusIndicator)}></div>}
-				/>
-				<TournamentStat
-					tag={'Prize Pool'}
-					value={'203,020 Ꝑ'}
-					icon={<HiOutlineTrophy className={cf(t.trophy)} />}
-				/>
-				<TournamentStat
-					tag={'Submission Due  Date'}
-					value={
-						<time dateTime='2023-06-23T12:00:00'>23rd June, 12:00 CAT</time>
-					}
-					icon={<FaRegFlag className={cf(t.flag)} />}
-				/>
-			</footer>
+				<footer className={cf(s.flex, s.flexStart, t.tournamentMeta)}>
+					<TournamentStat
+						tag={'Competition Status'}
+						value={'Active'}
+						icon={<div className={cf(t.statusIndicator)}></div>}
+					/>
+					<TournamentStat
+						tag={'Prize Pool'}
+						value={'203,020 Ꝑ'}
+						icon={<HiOutlineTrophy className={cf(t.trophy)} />}
+					/>
+					<TournamentStat
+						tag={'Submission Due  Date'}
+						value={
+							<time dateTime='2023-06-23T12:00:00'>23rd June, 12:00 CAT</time>
+						}
+						icon={<FaRegFlag className={cf(t.flag)} />}
+					/>
+				</footer>
+			</div>
 		</article>
 	)
 }
