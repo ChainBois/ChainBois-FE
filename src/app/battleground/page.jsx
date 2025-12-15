a355833f102ca59048863add65eb4c5f1e301c63
Atimscreative <timilehinsunday144@gmail.com>
2025-12-15 03:05:19 +0100

    feat: integrate Tailwind CSS and PostCSS for styling

---
+++ src/app/battleground/page.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function BattlegroundPage() {
+  return (
+    <>
+      <section>
+        <div className="container mx-auto p-4">
+          <div className="">
+            <h1 className="text-4xl font-bold text-white font-space-g">
+              Battleground
+            </h1>
+            <p className="text-white">
+              Compete in live tournaments and climb the leaderboard. Earn
+              $BATTLE, climb the ranks.
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
--- src/fonts/index.js
+++ src/fonts/index.js
@@ -1,32 +1,32 @@
-import { Inter, Space_Grotesk, Manrope, Anton } from 'next/font/google'
-import localFont from 'next/font/local'
+import { Inter, Space_Grotesk, Manrope, Anton } from "next/font/google";
+import localFont from "next/font/local";
 
 const inter = Inter({
-	subsets: ['latin'],
-	variable: '--font-inter',
-})
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 const spaceGrotesk = Space_Grotesk({
-	subsets: ['latin'],
-	variable: '--font-space-g',
-	weight: ['300', '400', '500', '600', '700'],
-})
+  subsets: ["latin"],
+  variable: "--font-space-g",
+  weight: ["300", "400", "500", "600", "700"],
+});
 
 const manrope = Manrope({
-	subsets: ['latin'],
-	variable: '--font-manrope',
-	weight: ['200', '300', '400', '500', '600', '700', '800'],
-})
+  subsets: ["latin"],
+  variable: "--font-manrope",
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+});
 
 const otomanopeeOne = localFont({
-	src: './OtomanopeeOne-Regular.ttf', // Path to your font file
-	variable: '--font-oto',
-})
+  src: "./OtomanopeeOne-Regular.ttf", // Path to your font file
+  variable: "--font-oto",
+});
 
 const anton = Anton({
-	subsets: ['latin'],
-	variable: '--font-anton',
-	weight: ['400'],
-})
+  subsets: ["latin"],
+  variable: "--font-anton",
+  weight: ["400"],
+});
 
-export { inter, spaceGrotesk, manrope, otomanopeeOne, anton }
+export { inter, spaceGrotesk, manrope, otomanopeeOne, anton };
