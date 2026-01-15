da88054228309e0eaed9bde9c05a4f5db787dc37
Emmanuel Agbavwe <agbavweisaac@gmail.com>
2026-01-15 21:16:23 +0100

    Revert "feat: add InventoryHero and MyWeapons components for inventory management"
    This reverts commit 28aca27923a5aa51ef2f45e434ac823b9db06fed.

--- src/app/inventory/page.jsx
+++ src/app/inventory/page.jsx
@@ -1,43 +1,5 @@
-import BorderedButton from "@/components/BorderedButton";
-import { Hero } from "@/components/Homepage";
-import { BsCaretRightFill } from "react-icons/bs";
-import Myweapons from "./components/Myweapons";
-import h from "@/components/Homepage/Hero.module.css";
+import React from "react";
 
-/**
- * Render the Inventory page containing a hero section and the user's weapons list.
- * @returns {JSX.Element} The Inventory page element with a Hero and Myweapons components.
- */
 export default function InventoryPage() {
-  return (
-    <>
-      <Hero
-        welcomeText={
-          <>
-            Welcome to the <br /> Inventory
-          </>
-        }
-        subText={
-          <>
-            Your complete collection of weapons, armor, and rewards.
-            <br />
-            Sell your weapons
-          </>
-        }
-        links={
-          <>
-            <BorderedButton
-              tag={"View Marketplace"}
-              isLink={true}
-              action={"#marketplace"}
-              icon={<BsCaretRightFill />}
-              borderButtonText={h.heroActionText}
-              borderButtonIcon={h.heroActionIcon}
-            />
-          </>
-        }
-      />
-      <Myweapons />
-    </>
-  );
+  return <div>page</div>;
 }
